ba954b8ed2db46d3747c70a4226282b6edf61a11
Zhang Zhenning <912106840628@njust.edu.cn>
2016-10-20 15:54:50 +0800

    Add files via upload

---
+++ dynamic_programming/53.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+        int n = nums.size();
+
+        int sum = nums[0];
+        int maxSum = sum;
+
+        for (int i = 1; i < n; i++) {
+           sum = max(nums[i], sum + nums[i]);
+           maxSum = max(maxSum, sum);
+        }
+
+         return maxSum;
+    }
+};
